3340878bf07c80d678bf6fd58d9872823a5166e0
Omar Osamah <Amoriimessi@users.noreply.github.com>
2020-10-29 17:16:05 +0300

    done

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,39 @@
-import Header from'../components/header'
-import Footer from'../components/footer'
-import MyCard from'../components/myCard'
-const Home=(props)=>{
+import Header from "../components/header";
+import Footer from "../components/footer";
+import MyCard from "../components/myCard";
+const Home = (props) => {
   // console.log(props);
-  // for checking the recieved data
-  return(
+  // for checking the received data
+  return (
     <>
-    <Header/>
-    <main>
-      <section className="home-cover">
-        <img src="../assets/home-cover.png"/>
-        <div className="overlay">
-          <div className="container">
-            <h1>Simple Blog.</h1>
-            <p>A blog created by FikraCamps</p>
+      <Header />
+      <main>
+        <section className="home-cover">
+          <img src="../assets/home-cover.png" />
+          <div className="overlay">
+            <div className="container">
+              <h1>Simple Blog.</h1>
+              <p>A blog created by FikraCamps</p>
+            </div>
           </div>
-        </div>
-      </section>
-      <section className="container blog-list">
-{props.posts.articles.map((article)=>(
- <MyCard key={article.createdAt} article={article} />
-))}
-         
-         
-      </section>
-      
-    </main>
-    <Footer/>
-
+        </section>
+        <section className="container blog-list">
+          {props.posts.articles.map((article) => (
+            <MyCard key={article.createdAt} article={article} />
+          ))}
+        </section>
+      </main>
+      <Footer />
     </>
   );
 };
 
 //////////////////////////////////////////////////////////////////////////////
 
-export async function getStaticProps(){
-  const res=await fetch(
-    "https://mashriq.herokuapp.com/dash/v1/articles"
-  );
-  const posts=await res.json();
-  return {props:{posts}};
+export async function getStaticProps() {
+  const res = await fetch("https://mashriq.herokuapp.com/dash/v1/articles");
+  const posts = await res.json();
+  return { props: { posts } };
 }
 
 export default Home;
